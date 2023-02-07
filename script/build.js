76783b065bc2c6f7f644e38d883bafc868d3bfc2
reuszeng <reuszeng@tencent.com>
2023-02-07 14:32:55 +0800

    feat: 压缩打包文件设为可选

--- script/build.js
+++ script/build.js
@@ -2,6 +2,7 @@ const rollup = require('rollup')
 const typescript = require('rollup-plugin-typescript2')
 const path = require('path')
 const uglify = require('rollup-plugin-uglify').uglify
+const inquirer = require('inquirer')
 
 const buildType = [
   {
@@ -22,29 +23,29 @@ function resolve(p) {
   return path.resolve(__dirname, '../', p)
 }
 
-function generateBuildPluginsConfigs() {
+function generateBuildPluginsConfigs(isMin) {
   const tsConfig = {
     verbosity: -1,
     tsconfig: path.resolve(__dirname, '../tsconfig.json'),
   }
-  return [typescript(tsConfig), uglify()]
+  const result = [typescript(tsConfig)];
+
+  if (isMin) {
+    result.push(uglify());
+  }
+
+  return result;
 }
 
-const configs = buildType.map((type) => {
+const generateConfig = (isMin) => buildType.map((type) => {
   const config = {
-    packageName: {
-      value: 'mouse-wheel'
-    },
-    ext: {
-      value: type.ext
-    },
     input: resolve('./src/index.ts'),
     output: {
       file: resolve(`dist/mouse-wheel${type.ext}`),
       name: 'mouse-wheel',
       format: type.format,
     },
-    plugins: generateBuildPluginsConfigs(),
+    plugins: generateBuildPluginsConfigs(isMin),
   }
 
   return config
@@ -54,9 +55,18 @@ function getSize(code) {
   return (code.length / 1024).toFixed(2) + 'kb'
 }
 
-const build = () => {
+const build = async () => {
+  const selected = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'min',
+      message: 'minify output files',
+    }
+  ]);
+
+  const configs = generateConfig(selected.min);
+
   configs.forEach((config) => {
-    console.log('config',config)
     rollup.rollup(config).then((bundle) => {
       bundle.write(config.output).then(({ output }) => {
         const code = output[0].code
